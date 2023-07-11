21dbf784a3c2617b2a6ca9e532ec0977d18b8f41
eduard-ops <epustynnik@gmail.com>
2023-07-11 16:21:42 +0300

    add

---
+++ nest-02/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+const PORT = 3000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  await app.listen(PORT);
+}
+
+bootstrap()
+  .then(() =>
+    console.log(
+      `Database connected successful, server started on port ${PORT}`,
+    ),
+  )
+  .catch((err) => console.log(err));
